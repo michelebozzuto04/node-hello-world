da7a1bf9bc86c25591e040328069a6501aec665a
michelebozzuto04 <michele.bozzuto04@gmail.com>
2025-06-26 16:21:35 +0200

    Refactor axios call to fetch data from API and log response

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ import chalk from "chalk";
 
 console.log(chalk.bgCyan("Hello Boolean"));
 console.log(chalk.underline("This is your current Env Variable:"), chalk.bgMagenta(process.env.ENV_VARIABLE));
+
+axios.get('https://lanciweb.github.io/demo/api/pictures/')
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
