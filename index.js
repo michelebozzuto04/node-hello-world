3eef0a79ed4dc3e32c896c3a47b33c393786a89a
michelebozzuto04 <michele.bozzuto04@gmail.com>
2025-06-26 16:00:45 +0200

    Add dotenv, axios, and chalk dependencies; update project files for environment variable support

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log("Hello Boolean");
+import axios from "axios";
+import chalk from "chalk";
+
+console.log(chalk.bgCyan("Hello Boolean"));
+console.log(chalk.underline("This is your current Env Variable:"), chalk.bgMagenta(process.env.ENV_VARIABLE));
