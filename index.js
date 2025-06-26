41589e2eb3c915ec411e423c781b8785c0113f89
michelebozzuto04 <michele.bozzuto04@gmail.com>
2025-06-26 16:31:15 +0200

    Update index.js to log API response data and modify package.json to use dotenv for environment variable support

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 import chalk from "chalk";
 
-console.log(chalk.bgCyan("Hello Boolean"));
+console.log(chalk.bgCyan(process.argv[2]));
 console.log(chalk.underline("This is your current Env Variable:"), chalk.bgMagenta(process.env.ENV_VARIABLE));
 
 axios.get('https://lanciweb.github.io/demo/api/pictures/')
     .then(function (response) {
-        console.log(response);
+        console.log(response.data);
     })
     .catch(function (error) {
         console.log(error);
